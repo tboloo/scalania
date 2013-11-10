3af6458dcfb8e468de7e4f1df0d3e359cc687928
Bolesław Tekielski <btekielski@gmail.com>
2013-11-10 16:59:52 +0100

    Solved first two exercises

--- exercises/src/main/scala/pl/japila/scalania/s99/S99_P02.scala
+++ exercises/src/main/scala/pl/japila/scalania/s99/S99_P02.scala
@@ -1,5 +1,9 @@
 package pl.japila.scalania.s99
 
 object S99_P02 {
-  def penultimate[T](ts: Seq[T]): T = ???
+  def penultimate[T](ts: Seq[T]): T = ts match {
+    case h :: t :: Nil => h
+    case h :: t => penultimate(t)
+  }
+
 }
--- exercises/src/main/scala/pl/japila/scalania/s99/S99_P03.scala
+++ exercises/src/main/scala/pl/japila/scalania/s99/S99_P03.scala
@@ -1,5 +1,8 @@
 package pl.japila.scalania.s99
 
 object S99_P03 {
-  def nth[T](k: Int, ts: Seq[T]): T = ???
+  def nth[T](k: Int, ts: Seq[T]): T = (k, ts) match {
+    case _ =>
+  }
+
 }
