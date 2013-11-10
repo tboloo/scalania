bda90205474100db63d19059ce88929add7bf7c2
Bolesław Tekielski <btekielski@gmail.com>
2013-11-10 17:53:13 +0100

    Solved exercise 5 and 6

--- exercises/src/main/scala/pl/japila/scalania/s99/S99_P05.scala
+++ exercises/src/main/scala/pl/japila/scalania/s99/S99_P05.scala
@@ -1,5 +1,9 @@
 package pl.japila.scalania.s99
 
 object S99_P05 {
-  def reverse[T](ts: Seq[T]): Seq[T] = ???
+  def reverse[T](ts: List[T]): List[T] = ts match {
+    case Nil => Nil
+    case h :: t => reverse(t) ::: List(h)
+  }
+
 }
