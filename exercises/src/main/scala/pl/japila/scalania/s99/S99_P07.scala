ba534daa5907b207ccd25d12df5957f17534cc52
Bolesław Tekielski <btekielski@gmail.com>
2013-11-11 13:38:34 +0100

    Implemented exercise 7

--- exercises/src/main/scala/pl/japila/scalania/s99/S99_P07.scala
+++ exercises/src/main/scala/pl/japila/scalania/s99/S99_P07.scala
@@ -1,5 +1,12 @@
 package pl.japila.scalania.s99
 
 object S99_P07 {
-  def flatten(ls: Seq[Any]): Seq[Any] = ???
+  def flatten(ls: Seq[Any]): Seq[Any] = ls match {
+    case Nil => Nil
+    case h :: t => h match {
+      case Nil => Nil
+      case x :: xs => x +: flatten(xs :+ flatten(t))
+      case _ => h +: flatten(t)
+    }
+  }
 }
