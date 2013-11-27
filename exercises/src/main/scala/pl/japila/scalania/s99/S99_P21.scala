5b949f5faecd528690535aaa6b3f402ebb9e6305
Bolesław Tekielski <btekielski@gmail.com>
2013-11-27 19:52:37 +0100

    P21, P22

--- exercises/src/main/scala/pl/japila/scalania/s99/S99_P21.scala
+++ exercises/src/main/scala/pl/japila/scalania/s99/S99_P21.scala
@@ -1,5 +1,8 @@
 package pl.japila.scalania.s99
 
 object S99_P21 {
-  def insertAt[T](toAdd: T, position: Int, ts: Seq[T]): Seq[T] = ???
+  def insertAt[T](toAdd: T, position: Int, ts: Seq[T]): Seq[T] = (position, ts) match {
+    case (0, h :: t) => Seq(toAdd) ++ t
+    case (position, h :: t) => Seq(h) ++ insertAt(toAdd, position - 1, t)
+  }
 }
--- exercises/src/main/scala/pl/japila/scalania/s99/S99_P22.scala
+++ exercises/src/main/scala/pl/japila/scalania/s99/S99_P22.scala
@@ -1,5 +1,8 @@
 package pl.japila.scalania.s99
 
 object S99_P22 {
-  def range(from: Int, to: Int): Seq[Int] = ???
+  def range(from: Int, to: Int): Seq[Int] = (from, to) match {
+    case (from, to) if from != to => Seq(from) ++ range(from + 1, to)
+    case _ => Nil
+  }
 }
