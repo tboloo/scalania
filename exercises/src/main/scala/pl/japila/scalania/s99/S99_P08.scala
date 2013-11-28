243f3de7c24eeb42111e04f50a93c41e782c9045
Bolesław Tekielski <btekielski@gmail.com>
2013-11-28 21:53:26 +0100

    P09

--- exercises/src/main/scala/pl/japila/scalania/s99/S99_P08.scala
+++ exercises/src/main/scala/pl/japila/scalania/s99/S99_P08.scala
@@ -3,6 +3,13 @@ package pl.japila.scalania.s99
 object S99_P08 {
   def compress[T](ts: Seq[T]): Seq[T] = ts match {
     case Nil => Nil
+    case _ => ts.foldLeft(Seq(ts.head))(
+      (acc, elem) => if (elem == acc.head) acc
+      else Seq(elem) ++ acc).reverse
+  }
+
+  def compress2[T](ts: Seq[T]): Seq[T] = ts match {
+    case Nil => Nil
     case h :: t => compressRec(h, t)
   }
   def compressRec[T](e: T, l: Seq[T]): Seq[T] = (e, l) match {
--- exercises/src/main/scala/pl/japila/scalania/s99/S99_P09.scala
+++ exercises/src/main/scala/pl/japila/scalania/s99/S99_P09.scala
@@ -1,5 +1,9 @@
 package pl.japila.scalania.s99
 
 object S99_P09 {
-  def pack[T](ts: Seq[T]): Seq[Seq[T]] = ???
+  def pack[T](ts: Seq[T]): Seq[Seq[T]] = ts match {
+    case Nil => Nil
+    case h :: t => t.foldLeft(Seq(Seq(h)))(
+      (acc, elem) => if (acc.head.head == elem) Seq(elem +: acc.head) ++ acc.tail else Seq(elem) +: acc).reverse
+  }
 }
