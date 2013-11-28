9817562e0e4a8ae10b9119dc7557e668a47d9773
Bolesław Tekielski <btekielski@gmail.com>
2013-11-28 22:10:11 +0100

    P10

--- exercises/src/main/scala/pl/japila/scalania/s99/S99_P10.scala
+++ exercises/src/main/scala/pl/japila/scalania/s99/S99_P10.scala
@@ -1,5 +1,11 @@
 package pl.japila.scalania.s99
 
 object S99_P10 {
-  def encode[T](ts: Seq[T]): Seq[(Int, T)] = ???
+  def encode[T](ts: Seq[T]): Seq[(Int, T)] = ts match {
+    case Nil => Nil
+    case h :: t => t.foldLeft(Seq((1, h)))(
+      (acc, elem) =>
+        if (acc.head._2 == elem) (acc.head._1 + 1, acc.head._2) +: acc.tail
+        else (1, elem) +: acc).reverse
+  }
 }
