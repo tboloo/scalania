12415c0500b3007cda49a17c9e31c2910a1a62d1
Bolesław Tekielski <btekielski@gmail.com>
2014-02-16 17:39:51 +0100

    P14, P18, P19

--- exercises/src/main/scala/pl/japila/scalania/s99/S99_P18.scala
+++ exercises/src/main/scala/pl/japila/scalania/s99/S99_P18.scala
@@ -1,5 +1,7 @@
 package pl.japila.scalania.s99
 
 object S99_P18 {
-  def slice[T](from: Int, to: Int, ts: Seq[T]): Seq[T] = ???
+  def slice[T](from: Int, to: Int, ts: Seq[T]): Seq[T] = {
+    (for { i <- from until to } yield ts(i)).toList
+  }
 }
