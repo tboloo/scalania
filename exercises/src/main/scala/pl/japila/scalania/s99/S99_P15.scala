4c7a94713b42114ae851dffd3fc6aae15682626f
Bolesław Tekielski <btekielski@gmail.com>
2014-02-16 09:51:42 +0100

    P15

--- exercises/src/main/scala/pl/japila/scalania/s99/S99_P15.scala
+++ exercises/src/main/scala/pl/japila/scalania/s99/S99_P15.scala
@@ -1,5 +1,10 @@
 package pl.japila.scalania.s99
 
 object S99_P15 {
-  def duplicateN[T](n: Int, ts: Seq[T]): Seq[T] = ???
+  def duplicateN[T](n: Int, ts: Seq[T]): Seq[T] = {
+    for {
+      e <- ts
+      i <- 1 to n
+    } yield e
+  }
 }
