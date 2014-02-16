4f0a8872a0ac113af2dbd162f9992a86d3e05b80
Bolesław Tekielski <btekielski@gmail.com>
2014-02-16 13:47:53 +0100

    P16, P17

--- exercises/src/main/scala/pl/japila/scalania/s99/S99_P16.scala
+++ exercises/src/main/scala/pl/japila/scalania/s99/S99_P16.scala
@@ -1,5 +1,9 @@
 package pl.japila.scalania.s99
 
 object S99_P16 {
-  def drop[T](n: Int, ts: Seq[T]): Seq[T] = ???
+  def drop[T](n: Int, ts: Seq[T]): Seq[T] = {
+    (for {
+      i <- 1 to ts.size if i % n != 0
+    } yield ts(i - 1)).toList
+  }
 }
--- exercises/src/main/scala/pl/japila/scalania/s99/S99_P17.scala
+++ exercises/src/main/scala/pl/japila/scalania/s99/S99_P17.scala
@@ -1,5 +1,5 @@
 package pl.japila.scalania.s99
 
 object S99_P17 {
-  def split[T](n: Int, ts: Seq[T]): (Seq[T], Seq[T]) = ???
+  def split[T](n: Int, ts: Seq[T]): (Seq[T], Seq[T]) = (ts take n, ts drop n)
 }
